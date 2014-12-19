631c7591bba59578a2e9573f2fd3356a0cdbdd58
rxk123456 <rxk@wp.pl>
2014-12-20 00:08:15 +0100

    Initial implementation
    first attempt to implement assignment functions

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,32 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(matrixData = matrix()) {
+    solvedMatrix <- NULL
+    set <- function(data) {
+        matrixData <<- data
+        solvedMatrix <<- NULL
+    }
+    get <- function() matrixData
+    setSolution <- function(solution) solvedMatrix <<- solution
+    getSolution <- function() solvedMatrix
+    list(set = set, get = get,
+         setSolution = setSolution,
+         getSolution = getSolution)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(cacheMatrix, ...) {
         ## Return a matrix that is the inverse of 'x'
+    s <- cacheMatrix$getSolution()
+    if(!is.null(s)) {
+        message("getting cached solution")
+        return(s)
+    }
+    data <- cacheMatrix$get()
+    s <- solve(data, ...)
+    cacheMatrix$setSolution(s)
+    s
 }
